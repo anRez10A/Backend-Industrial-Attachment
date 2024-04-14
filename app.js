2b5a9897e33267cda4c925970e8623e19b3f3ab6
Shamanta Rezwana <srezwana4@gmail.com>
2024-04-15 04:26:41 +0600

    Login Aurization done

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const authMiddleware = require('./middleware/authMiddleware.js');
+
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = "toDo";
 
 
-
-
+dotenv.config();
 const app = express();
 
 
@@ -26,7 +30,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 const uri = "mongodb+srv://Srezwana:u2zNhIFKjm9yCDYP@cluster0.zttubvy.mongodb.net/";
 const client = new MongoClient(uri); 
 
-app.get('/recipes', async (req, res, next) => {
+app.get('/recipes',authMiddleware, async (req, res, next) => {
    
         await client.connect();
         const database = client.db('recipeBook');
@@ -36,7 +40,7 @@ app.get('/recipes', async (req, res, next) => {
    
 });
 
-app.post('/recipes', async (req, res, next) => {
+app.post('/recipes',authMiddleware, async (req, res, next) => {
   const { recipeName,ingredients, description, category} = req.body;
   try {
       await client.connect();
@@ -54,4 +58,91 @@ app.post('/recipes', async (req, res, next) => {
     } finally {
       // await client.close();
     }
+});
+
+app.delete('/recipes/:id', authMiddleware,async (req, res, next) => {
+  const { id: recipeIdToDelete } = req.params;
+  try {
+      await client.connect();
+      const database = client.db('recipeBook');
+      const recipeCollection = database.collection('recipe');
+      const query = {"_id": new ObjectId(recipeIdToDelete)};
+      const deleteResult = await recipeCollection.deleteOne(query);
+      return res.status(200).json(deleteResult);
+  } finally {
+      //await client.close();
+  }
+});
+
+app.post('/users', async (req, res, next) => {
+    const { userName, email, password} = req.body;
+    try {
+        await client.connect();
+        const database = client.db('recipeBook');
+        const userCollection = database.collection('users');
+        
+        const recipe = await userCollection.insertOne({
+            userName,
+            email,
+            password
+        });
+  
+        return res.status(200).json(recipe);
+      } finally {
+        //await client.close();
+      }
+  });
+
+  // logIn Authentication
+
+  const TOKEN_KEY = 'ABCXYZ';
+
+app.post('/login', async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        await client.connect();
+        const database = client.db('recipeBook');
+        const userCollection = database.collection('users');
+        console.log(email);
+        const user = await userCollection.findOne({email});
+        if(user){
+            console.log("success");
+            if(user.password === password){
+                console.log("Password Checked");
+                const data = {
+                    users:{id: user.id}
+                };
+
+                const authToken = jwt.sign(
+                    data,
+                    TOKEN_KEY,
+  
+                );
+
+                console.log('Generated Token:', authToken); // Log the token
+
+                return res.json({token: authToken, success: true});
+                
+            }
+            else{
+                return res.json({token: null, success: false});
+            }
+        }
+
+        
+        else{
+            return res.json({massage: 'Invalid UserName or Password', success: false});
+        }
+
+      } finally {
+        await client.close();
+      }
+
+      
+});
+
+app.get('/login/customer', authMiddleware, async (req, res, next) => {
+
+     return res.json({checked: 'true'});
+
 });
